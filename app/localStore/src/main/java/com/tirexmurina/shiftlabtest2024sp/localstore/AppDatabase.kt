164e1bebe8cc155dcf439dc2a82f56d0ad510dcd
tireks <33616425+tireks@users.noreply.github.com>
2024-04-15 15:07:47 +0700

    module especially for local database

---
+++ app/localStore/src/main/java/com/tirexmurina/shiftlabtest2024sp/localstore/AppDatabase.kt
@@ -0,0 +1,31 @@
+package com.tirexmurina.shiftlabtest2024sp.localstore
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.tirexmurina.shiftlabtest2024sp.localstore.model.UserModelLocal
+
+@Database(entities = [UserModelLocal::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase(){
+
+    abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+        // Функция для получения экземпляра базы данных.
+        fun getDatabase(context: Context): AppDatabase {
+            // Если база данных еще не инициализирована, то инициализируем ее.
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
