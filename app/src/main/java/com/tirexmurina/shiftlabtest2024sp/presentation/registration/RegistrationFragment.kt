b2bd02de2d419f5e5c2e2e44cfa6813c866a9bd6
tireks <33616425+tireks@users.noreply.github.com>
2024-04-15 16:35:22 +0700

    hiltViewModels and entry points

--- app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/registration/RegistrationFragment.kt
+++ app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/registration/RegistrationFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tirexmurina.shiftlabtest2024sp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RegistrationFragment : Fragment() {
 
 }
