5ea8cf7fddecd797a2ff2fe142659cbd5d412d46
tireks <33616425+tireks@users.noreply.github.com>
2024-04-17 14:42:48 +0700

    states for mvvm

---
+++ app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/home/RegistrationState.kt
@@ -0,0 +1,31 @@
+package com.tirexmurina.shiftlabtest2024sp.presentation.home
+
+import com.tirexmurina.shiftlabtest2024sp.utils.UserParam
+
+sealed interface RegistrationState {
+
+    data object Initial : RegistrationState
+
+    data object Loading : RegistrationState
+
+    data object Skip : RegistrationState
+
+    sealed interface Error : RegistrationState {
+
+        data object EmptyTable : Error
+
+        data object CorruptedDataFromDataSource : Error
+
+        data object Unknown : Error
+
+        data object CorruptedDataFromForm : Error
+    }
+
+    sealed interface Content : RegistrationState {
+
+        data class Locked(val troubleList: List<UserParam>) : Content
+
+        data object Unlocked : Content
+    }
+
+}
