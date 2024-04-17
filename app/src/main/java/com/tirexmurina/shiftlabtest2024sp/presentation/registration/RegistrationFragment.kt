2bc15e06907f69889fcd910bcb02b41f2844d9d9
tireks <33616425+tireks@users.noreply.github.com>
2024-04-17 14:47:24 +0700

    start boilerplate code

--- app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/registration/RegistrationFragment.kt
+++ app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/registration/RegistrationFragment.kt
@@ -1,14 +1,48 @@
 package com.tirexmurina.shiftlabtest2024sp.presentation.registration
 
+import android.content.res.ColorStateList
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.tirexmurina.shiftlabtest2024sp.R
+import com.tirexmurina.shiftlabtest2024sp.databinding.FragmentRegistrationBinding
+import com.tirexmurina.shiftlabtest2024sp.presentation.BaseFragment
+import com.tirexmurina.shiftlabtest2024sp.presentation.home.RegistrationState
+import com.tirexmurina.shiftlabtest2024sp.utils.AppTextWatcher
+import com.tirexmurina.shiftlabtest2024sp.utils.UserParam
+import com.tirexmurina.shiftlabtest2024sp.utils.mainActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
-class RegistrationFragment : Fragment() {
+class RegistrationFragment : BaseFragment<FragmentRegistrationBinding>() {
+
+    private val viewModel : RegistrationViewModel by viewModels()
+
+    // наследование от BaseFragment в этом фрагменте -
+    // исключительно чтобы избавиться от бойлерплэйта,
+    // связанного с viewbinding в фрагментах
+
+    override fun inflateViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentRegistrationBinding {
+        return FragmentRegistrationBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.state.observe(viewLifecycleOwner, ::handleState)
+        initializeScreen()
+    }
 
 }
