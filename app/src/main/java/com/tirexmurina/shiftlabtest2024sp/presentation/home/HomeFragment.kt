2ea7e064ac12cfcba18ce4610e5736f57945aaf6
tireks <33616425+tireks@users.noreply.github.com>
2024-04-17 21:01:20 +0700

    start boilerplate

--- app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/home/HomeFragment.kt
@@ -1,12 +1,23 @@
 package com.tirexmurina.shiftlabtest2024sp.presentation.home
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import com.tirexmurina.shiftlabtest2024sp.R
 import com.tirexmurina.shiftlabtest2024sp.databinding.FragmentHomeBinding
+import com.tirexmurina.shiftlabtest2024sp.domain.entity.User
 import com.tirexmurina.shiftlabtest2024sp.presentation.BaseFragment
+import com.tirexmurina.shiftlabtest2024sp.utils.mainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,4 +36,9 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         return FragmentHomeBinding.inflate(inflater, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.state.observe(viewLifecycleOwner, ::handleState)
+        initializeScreen()
+    }
 }
