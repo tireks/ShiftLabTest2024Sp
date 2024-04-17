162bd93b20044113b4ea0d107fc15a598197a998
tireks <33616425+tireks@users.noreply.github.com>
2024-04-17 14:41:32 +0700

    placeholders

--- app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/home/HomeFragment.kt
@@ -1,12 +1,28 @@
 package com.tirexmurina.shiftlabtest2024sp.presentation.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.tirexmurina.shiftlabtest2024sp.R
+import androidx.fragment.app.viewModels
+import com.tirexmurina.shiftlabtest2024sp.databinding.FragmentHomeBinding
+import com.tirexmurina.shiftlabtest2024sp.presentation.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment() {
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+
+    private val viewModel : HomeViewModel by viewModels()
+
+    // наследование от BaseFragment в этом фрагменте -
+    // исключительно чтобы избавиться от бойлерплэйта,
+    // связанного с viewbinding в фрагментах
+
+    override fun inflateViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentHomeBinding {
+        return FragmentHomeBinding.inflate(inflater, container, false)
+    }
 
 }
--- app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/tirexmurina/shiftlabtest2024sp/presentation/home/HomeViewModel.kt
@@ -1,8 +1,21 @@
 package com.tirexmurina.shiftlabtest2024sp.presentation.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tirexmurina.shiftlabtest2024sp.domain.usecase.DeleteUserUseCase
+import com.tirexmurina.shiftlabtest2024sp.domain.usecase.GetUserUseCase
+import com.tirexmurina.shiftlabtest2024sp.domain.usecase.SaveUserUseCase
+import com.tirexmurina.shiftlabtest2024sp.utils.UserParam
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 @HiltViewModel
-class HomeViewModel {
+class HomeViewModel @Inject constructor(
+
+) : ViewModel() {
+
 }
